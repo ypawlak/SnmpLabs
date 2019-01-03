8eb7065169502c3b893a258bd56dc959889dbe27
ypawlak <ypawlak@outlook.com>
2019-01-03 18:24:16 +0100

    empty test changed to empty parameter test

--- SmiParser.Tests/AliasesParserTests.cs
+++ SmiParser.Tests/AliasesParserTests.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Linq;
 using Xunit;
 using SmiParser;
 
@@ -6,9 +11,11 @@ namespace SmiParser.Tests
     public class AliasesParserTests
     {
         [Fact]
-        public void FirstTest()
+        public void EmptyTest()
         {
-            
+            IEnumerable<AliasInfo> emptyResult = AliasesParser.Parse("");
+            Assert.NotNull(emptyResult);
+            Assert.Empty(emptyResult);
         }
     }
 }
