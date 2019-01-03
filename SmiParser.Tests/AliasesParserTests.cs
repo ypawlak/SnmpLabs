f9e031d8d2675d40b53ba1e0c429a76226c3c12a
ypawlak <ypawlak@outlook.com>
2019-01-03 18:17:18 +0100

    Added unit tests project for parser

---
+++ SmiParser.Tests/AliasesParserTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using SmiParser;
+
+namespace SmiParser.Tests
+{
+    public class AliasesParserTests
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            
+        }
+    }
+}
