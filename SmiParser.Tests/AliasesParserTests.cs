8d32901854676b6a3aef401d620e0feae330b2f8
ypawlak <ypawlak@outlook.com>
2019-01-04 09:18:24 +0100

    Added whole file parsing test for aliases, and separate method for isolated types matching

--- SmiParser.Tests/AliasesParserTests.cs
+++ SmiParser.Tests/AliasesParserTests.cs
@@ -20,23 +20,50 @@ namespace SmiParser.Tests
             Assert.Empty(emptyResult);
         }
 
-        public static IEnumerable<object[]> ExampleMibCases =>
+
+        [Theory]
+        [MemberData(nameof(IsolatedTypesCases))]
+        public void IsolatedTypesTest(string input, AliasInfo expected)
+        {
+            IEnumerable<AliasInfo> executionResult = AliasesParser.Parse(input);
+            Assert.Single(executionResult);
+            Assert.Equal(expected, executionResult.Single(), new AliasInfoValueEqualityComparer());
+        }
+
+        public static IEnumerable<object[]> IsolatedTypesCases =>
         new List<object[]>
         {
             new object[] {
+                "DisplayString ::= OCTET STRING",
+                new AliasInfo
+                    {
+                        Alias = "DisplayString",
+                        OriginalBaseTypeName = "OCTET STRING",
+                        OriginalComplexTypeName = string.Empty 
+                    }
+            },
+            new object[] {
                 @"
-DisplayString ::=
-    OCTET STRING",
-                new List<AliasInfo>
-                {
-                    new AliasInfo
+                    IpAddrEntry ::=
+                        SEQUENCE {
+                            ipAdEntAddr
+                                IpAddress,
+                            ipAdEntIfIndex
+                                INTEGER,
+                            ipAdEntNetMask
+                                IpAddress,
+                            ipAdEntBcastAddr
+                                INTEGER,
+                            ipAdEntReasmMaxSize
+                                INTEGER (0..65535)
+                }",
+                new AliasInfo
                         {
-                            Alias = "DisplayString",
-                            OriginalBaseTypeName = "OCTET STRING",
+                            Alias = "IpAddrEntry",
+                            OriginalBaseTypeName = "SEQUENCE",
                             OriginalComplexTypeName = string.Empty 
                         }
                 }
-            }
         };
 
         [Theory]
@@ -46,5 +73,141 @@ DisplayString ::=
             IEnumerable<AliasInfo> executionResult = AliasesParser.Parse(input);
             Assert.Equal(expectedResult, executionResult, new AliasInfoValueEqualityComparer());
         }
+
+        public static IEnumerable<object[]> ExampleMibCases =>
+        new List<object[]>
+        {
+            new object[] {
+                @"RFC1155-SMI DEFINITIONS ::= BEGIN
+
+                    EXPORTS -- EVERYTHING
+                            internet, directory, mgmt,
+                            experimental, private, enterprises,
+                            OBJECT-TYPE, ObjectName, ObjectSyntax, SimpleSyntax,
+                            ApplicationSyntax, NetworkAddress, IpAddress,
+                            Counter, Gauge, TimeTicks, Opaque;
+
+                    -- the path to the root
+
+                    internet      OBJECT IDENTIFIER ::= { iso org(3) dod(6) 1 }
+
+                    directory     OBJECT IDENTIFIER ::= { internet 1 }
+
+                    mgmt          OBJECT IDENTIFIER ::= { internet 2 }
+
+                    experimental  OBJECT IDENTIFIER ::= { internet 3 }
+
+                    private       OBJECT IDENTIFIER ::= { internet 4 }
+                    enterprises   OBJECT IDENTIFIER ::= { private 1 }
+
+                    -- definition of object types
+
+                    OBJECT-TYPE MACRO ::=
+                    BEGIN
+                        TYPE NOTATION ::= ""SYNTAX"" type (TYPE ObjectSyntax)
+                                        ""ACCESS"" Access
+                                        ""STATUS"" Status
+                        VALUE NOTATION ::= value (VALUE ObjectName)
+
+                        Access ::= ""read-only""
+                                        | ""read-write""
+                                        | ""write-only""
+                                        | ""not-accessible""
+                        Status ::= ""mandatory""
+                                        | ""optional""
+                                        | ""obsolete""
+                    END
+
+                        -- names of objects in the MIB
+
+                        ObjectName ::=
+                            OBJECT IDENTIFIER
+
+                        -- syntax of objects in the MIB
+
+                        ObjectSyntax ::=
+                            CHOICE {
+                                simple
+                                    SimpleSyntax,
+                        -- note that simple SEQUENCEs are not directly
+                        -- mentioned here to keep things simple (i.e.,
+                        -- prevent mis-use).  However, application-wide
+                        -- types which are IMPLICITly encoded simple
+                        -- SEQUENCEs may appear in the following CHOICE
+
+                                application-wide
+                                    ApplicationSyntax
+                            }
+
+                        SimpleSyntax ::=
+                            CHOICE {
+                                number
+                                    INTEGER,
+                                string
+                                    OCTET STRING,
+                                object
+                                    OBJECT IDENTIFIER,
+                                empty
+                                    NULL
+                            }
+
+                        ApplicationSyntax ::=
+                            CHOICE {
+                                address
+                                    NetworkAddress,
+                                counter
+                                    Counter,
+                                gauge
+                                    Gauge,
+                                ticks
+                                    TimeTicks,
+                                arbitrary
+                                    Opaque
+
+                        -- other application-wide types, as they are
+                        -- defined, will be added here
+                            }
+
+                        -- application-wide types
+
+                        NetworkAddress ::=
+                            CHOICE {
+                                internet
+                                    IpAddress
+                            }
+
+                        IpAddress ::=
+                            [APPLICATION 0]          -- in network-byte order
+                                IMPLICIT OCTET STRING (SIZE (4))
+
+                        Counter ::=
+                            [APPLICATION 1]
+                                IMPLICIT INTEGER (0..4294967295)
+
+                        Gauge ::=
+                            [APPLICATION 2]
+                                IMPLICIT INTEGER (0..4294967295)
+
+                        TimeTicks ::=
+                            [APPLICATION 3]
+                                IMPLICIT INTEGER (0..4294967295)
+
+                        Opaque ::=
+                            [APPLICATION 4]          -- arbitrary ASN.1 value,
+                                IMPLICIT OCTET STRING   --   ""double-wrapped""
+
+                        END
+                    ",
+                new List<AliasInfo>
+                {
+                    new AliasInfo
+                        {
+                            Alias = "ObjectName",
+                            OriginalBaseTypeName = "OBJECT IDENTIFIER",
+                            OriginalComplexTypeName = string.Empty 
+                        }
+                }
+            }
+        };
     }
 }
